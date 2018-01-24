96d57b62e4994161b168c2d8a66166abf4726088
Harleigh Abel <harleighabel@gmail.com>
2018-01-24 17:31:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,14 @@
-def fizzbuzz
-
+def fizzbuzz(n)
+  if (n % 3) == 0 && (n % 5) != 0 
+    return "Fizz"
+  elseif
+    (n % 3) != 0 && (n % 5) == 0 
+    return "Buzz"
+  elseif
+    (n % 3) == 0 && (n % 5) == 0 
+    return "FizzBuzz"
+  else
+    return nil    
+  end
 end
   
